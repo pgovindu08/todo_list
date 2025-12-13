016b784f36f529ebd2ce1ca13bbdf512dda3d7fb
pgovindu08 <pssg2103@gmail.com>
2025-12-12 23:30:34 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './Button.jsx'
+
+function App() {
+
+  return (
+    <>
+      <title>My Website</title>
+      <Button></Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+function Button(){
+    return(
+        <>
+            <div className="wrapper">
+                <button className="button">Home</button>
+                <button className="button">About</button>
+                <button className="button">About</button>
+            </div>
+        </>
+    );
+}
+
+export default Button
